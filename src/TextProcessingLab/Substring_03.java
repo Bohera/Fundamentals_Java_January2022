7009544476bd11e1b518feb376e1a224458e8338
Boris Mitov <boris.m.mitov@gmail.com>
2022-03-14 21:22:56 +0200

    3 zadachi ot Text Processing - ostava edna

--- src/TextProcessingLab/Substring_03.java
+++ src/TextProcessingLab/Substring_03.java
@@ -6,7 +6,14 @@ public class Substring_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String firstText = scanner.nextLine();
+        String secondText = scanner.nextLine();
 
+        while (secondText.contains(firstText)){
+            secondText = secondText.replace(firstText, "");
+        }
+
+        System.out.println(secondText);
 
     }
 }
--- src/TextProcessingLab/TextFilter_04.java
+++ src/TextProcessingLab/TextFilter_04.java
@@ -6,7 +6,15 @@ public class TextFilter_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] bannedList = scanner.nextLine().split(", ");
+        String text = scanner.nextLine();
 
+        for (int i = 0; i < bannedList.length; i++) {
+            int length = bannedList[i].length();
+            String censor = "*";
 
+            text = text.replaceAll(bannedList[i], censor.repeat(length));
+        }
+        System.out.println(text);
     }
 }
