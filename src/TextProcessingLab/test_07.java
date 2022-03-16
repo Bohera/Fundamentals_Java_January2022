35e46c7af077d2a6044a5c5a95a9213870b08942
Boris Mitov <boris.m.mitov@gmail.com>
2022-03-16 23:36:48 +0200

    Poslednite zadachi ot Text processing

---
+++ src/TextProcessingLab/test_07.java
@@ -0,0 +1,18 @@
+package TextProcessingLab;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class test_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(new Date());
+        StringBuilder text = new StringBuilder();
+
+        for (int i = 0; i < 1000000000; i++) {
+            text.append("a");
+        }
+        System.out.println(new Date());
+    }
+}
