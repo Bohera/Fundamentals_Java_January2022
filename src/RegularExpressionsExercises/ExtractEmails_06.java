6080ec2efa435d5b3053440f77373cabf73c8a71
Boris Mitov <boris.m.mitov@gmail.com>
2022-03-23 18:18:20 +0200

    Text Processing Lab split s while ne s regex

--- src/RegularExpressionsExercises/ExtractEmails_06.java
+++ src/RegularExpressionsExercises/ExtractEmails_06.java
@@ -1,13 +1,22 @@
 package RegularExpressionsExercises;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExtractEmails_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String line = scanner.nextLine();
 
+        String regexEmail = "(?<email>[A-Za-z0-9._-]+@[A-Za-z-.]*[A-Za-z]+)";  //[^._ -]+ \b(?<email>[A-Za-z0-9._-]+@[A-Za-z-.]+[A-Za-z]+)\b
+        Pattern emailPattern = Pattern.compile(regexEmail);
+        Matcher emailMatcher = emailPattern.matcher(line);
 
+        while (emailMatcher.find()) {
+            System.out.println(emailMatcher.group("email"));
+        }
 
     }
 }
