fefed3f6fa8d0d0d26700fef3c7365044d2b805e
Boris Mitov <boris.m.mitov@gmail.com>
2022-04-03 13:46:44 +0300

    Final Exam Fundamentals April 2022

---
+++ src/FinalExam03April2022/test.java
@@ -0,0 +1,20 @@
+package FinalExam03April2022;
+
+public class test {
+    public static void main(String[] args) {
+        boolean isTrue = 100f == 100d;
+        System.out.println(isTrue);
+
+
+        int a = 5;
+        int b = a++;
+        int c = ++a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+
+    }
+
+}
